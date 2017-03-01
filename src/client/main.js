3b4aec9f6a1e6614a863e3314463c3c80b4e3eda
Jurgen Schoorlemmer <jurgen@homero.nl>
2017-03-01 08:32:17 +0100

    basic

--- src/client/main.js
+++ src/client/main.js
@@ -4,7 +4,7 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
+  // counter starts at 0 
   this.counter = new ReactiveVar(0);
 });
 
